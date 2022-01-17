646d3df9050a150114ea20142b4961a78b886641
Sunil <sunilswizy18@gmail.com>
2022-01-17 07:53:19 +0530

    Adding color palette!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Palette from "./components/palette/palette.component";
+import seedColors from "./components/Seed-colors/seedcolor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<Palette {...seedColors[1]} />
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/palette/palette.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./palette.styles.css";
+
+class Palette extends React.Component {
+	render() {
+		return (
+			<div className='Palette'>
+				<div className='Palette-colors'></div>
+			</div>
+		);
+	}
+}
+
+export default Palette;
